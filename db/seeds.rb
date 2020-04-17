429c0c7fed7dc478fe00d06c039a5bb6d1df6127
Dylan Salay <dylansalay@Dylans-MacBook-Pro.local>
2020-04-17 15:57:32 -0700

    Implemented user/post connection with tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,9 @@
 end
 
 puts "100 posts have been created"
+
+1.times do |user|
+  User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+end
+
+puts "Jon Snow has been created"
